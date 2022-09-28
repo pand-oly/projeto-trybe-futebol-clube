977430e04c52dfbcaca2d8a6923eea1ec87087d9
pand-oly <hoa98hoa@gmail.com>
2022-09-28 14:38:03 -0300

    feat: Created migration create-teams and the model TeamModel rename UserModel and seeder teams

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,24 @@
+import { Model, STRING } from 'sequelize';
+import db from '.';
+import Matche from './MatcheModel';
+
+class Team extends Model {
+  id!: number;
+  teamName: string;
+}
+
+Team.init({
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'team',
+  timestamps: false,
+});
+
+Team.hasMany(Matche, { foreignKey: 'id', as: 'matche' });
+
+export default Team;
