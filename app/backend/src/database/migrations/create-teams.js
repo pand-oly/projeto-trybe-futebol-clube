f32d866af76952b7d8efd808a3fe4d21330fee3f
pand-oly <hoa98hoa@gmail.com>
2022-09-28 14:38:03 -0300

    feat: Created migration create-teams and the model TeamModel rename UserModel and seeder teams

---
+++ app/backend/src/database/migrations/create-teams.js
@@ -0,0 +1,19 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      team_name: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
