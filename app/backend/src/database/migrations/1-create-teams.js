ce57e46cd12a2bc58531046102bea73f1c415384
pand-oly <hoa98hoa@gmail.com>
2022-09-28 18:52:54 -0300

    fix: renamed create-teams migration so it can be created before the matches table

--- app/backend/src/database/migrations/1-create-teams.js
+++ app/backend/src/database/migrations/1-create-teams.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('users', {
+    await queryInterface.createTable('teams', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -14,6 +14,6 @@ module.exports = {
     });
   },
   down: async (queryInterface) => {
-    await queryInterface.dropTable('users');
+    await queryInterface.dropTable('teams');
   },
 };
