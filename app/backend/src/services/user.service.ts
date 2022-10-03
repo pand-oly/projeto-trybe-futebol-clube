f5b67dab6b1fa8535afadc0b5e531743e67a542b
pand-oly <hoa98hoa@gmail.com>
2022-10-03 06:01:43 -0300

    feat: created method login in user service

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -1,10 +1,23 @@
+import { compare } from 'bcryptjs';
 import UserModel from '../model/user.model';
-import IUser from '../interfaces/IUser';
+import IUser, { ILogin } from '../interfaces/IUser';
+import jwtService from './helpers/jwt.service';
+import CustomError from '../error';
 
 export default class UserService {
   constructor(private userModel: UserModel) {}
 
   public async findOne(email: string): Promise<IUser> {
     return this.userModel.findOne(email);
+  }
+
+  public async login(params: ILogin): Promise<string> {
+    const email = await this.userModel.findOne(params.email);
+
+    if (await compare(params.password, email.password)) {
+      const token = jwtService.generate(params);
+      return token;
+    }
+    throw new CustomError(400, 'email or password invalid');
   }
 }
