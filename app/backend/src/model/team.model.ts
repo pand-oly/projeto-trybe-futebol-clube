065b0d724c0e0663b09d55771a835c9c58005e49
pand-oly <hoa98hoa@gmail.com>
2022-10-04 11:50:45 -0300

    feat: Created team model

---
+++ app/backend/src/model/team.model.ts
@@ -0,0 +1,16 @@
+import ITeam from '../interfaces/ITeam';
+import teamModelDB from '../database/models/TeamModel';
+import CustomError from '../error';
+
+export default class TeamModel {
+  private model = teamModelDB;
+
+  public findAll = async (): Promise<ITeam[]> => {
+    try {
+      const result = await this.model.findAll();
+      return result;
+    } catch (error) {
+      throw new CustomError(500, 'Erro database');
+    }
+  };
+}
