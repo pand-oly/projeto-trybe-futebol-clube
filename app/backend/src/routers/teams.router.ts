364e7396b2290261e5653f4394082fbf637e1e39
pand-oly <hoa98hoa@gmail.com>
2022-10-04 14:56:12 -0300

    feat: Create route team

--- app/backend/src/routers/teams.router.ts
+++ app/backend/src/routers/teams.router.ts
@@ -1,5 +1,14 @@
 import { Router } from 'express';
+import TeamController from '../controllers/team.controller';
+import TeamModel from '../model/team.model';
+import TeamService from '../services/team.service';
 
 const teamsRouter = Router();
+
+const teamModel = new TeamModel();
+const teamService = new TeamService(teamModel);
+const teamController = new TeamController(teamService);
+
+teamsRouter.get('/', teamController.findAll);
 
 export default teamsRouter;
