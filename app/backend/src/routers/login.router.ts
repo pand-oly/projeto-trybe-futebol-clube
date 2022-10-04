ee5eb3df3d7b4184182e83ab7dcf3a30d82d2d51
pand-oly <hoa98hoa@gmail.com>
2022-10-04 11:25:46 -0300

    feat: New route get /login/validate

--- app/backend/src/routers/login.router.ts
+++ app/backend/src/routers/login.router.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import UserController from '../controllers/user.controller';
 import UserModel from '../model/user.model';
 import UserService from '../services/user.service';
-import loginValidate from '../middleware/loginValidate';
+import checkLoginBody from '../middleware/loginValidate';
 
 const loginRouter = Router();
 
@@ -10,6 +10,7 @@ const userModel = new UserModel();
 const userService = new UserService(userModel);
 const userController = new UserController(userService);
 
-loginRouter.post('/', loginValidate, (req, res, next) => userController.login(req, res, next));
+loginRouter.post('/', checkLoginBody, (req, res, next) => userController.login(req, res, next));
+loginRouter.get('/validate', (req, res, next) => userController.loginValidate(req, res, next));
 
 export default loginRouter;
