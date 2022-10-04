77f5289202b3f15c6c5e2cf1577a59e7186fd5ae
pand-oly <hoa98hoa@gmail.com>
2022-10-04 11:22:30 -0300

    fix: Add try/catch case don't user valide

--- app/backend/src/model/user.model.ts
+++ app/backend/src/model/user.model.ts
@@ -1,11 +1,16 @@
 import IUser from '../interfaces/IUser';
 import userModelDB from '../database/models/UserModel';
+import CustomError from '../error';
 
 export default class UserModel {
   private model = userModelDB;
 
   public async findOne(email: string): Promise<IUser> {
-    const result = await this.model.findOne({ where: { email } });
-    return result as IUser;
+    try {
+      const result = await this.model.findOne({ where: { email } });
+      return result as IUser;
+    } catch (error) {
+      throw new CustomError(400, 'non-existent user');
+    }
   }
 }
