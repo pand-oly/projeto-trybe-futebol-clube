1a503c61f65cd79351268eb13c5bfa86e7c363c6
pand-oly <hoa98hoa@gmail.com>
2022-10-05 18:29:57 -0300

    fix: Add msg erro case string empty

--- app/backend/src/middleware/loginValidate.ts
+++ app/backend/src/middleware/loginValidate.ts
@@ -3,14 +3,18 @@ import Joi = require('joi');
 import CustomError from '../error';
 import { ILogin } from '../interfaces/IUser';
 
+const MSG_ERROR = '400 / All fields must be filled';
+
 const schemaBody = Joi.object<ILogin>({
   email: Joi.string().email().required().messages({
-    'any.required': '400 / All fields must be filled',
+    'any.required': MSG_ERROR,
     'string.email': '400 / "email" must be a valid email',
+    'string.empty': MSG_ERROR,
   }),
   password: Joi.string().min(6).required().messages({
-    'any.required': '400 / All fields must be filled',
+    'any.required': MSG_ERROR,
     'string.min': '401 / "password" length must be at least 6 characters long',
+    'string.empty': MSG_ERROR,
   }),
 });
 
@@ -18,7 +22,7 @@ export default function checkLoginBody(req: Request, _res: Response, next: NextF
   const { error } = schemaBody.validate(req.body);
   if (error) {
     console.log('Login validate error:\n', error);
-    const [code, message] = error.message.split(' / ');
+    const [code, message] = error.details[0].message.split(' / ');
     throw new CustomError(+code, message);
   }
   next();
