86e55a1068c7120fc5b2ce4a32e3526776e77248
pand-oly <hoa98hoa@gmail.com>
2022-10-06 12:24:27 -0300

    set status code

--- app/backend/src/controllers/matche.controller.ts
+++ app/backend/src/controllers/matche.controller.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import MatcheService from '../services/matche.service';
 import auth from '../helpers/auth';
-import CustomError from '../error';
 
 export default class MatcheController {
   constructor(private matcheService: MatcheService) {}
@@ -16,14 +15,14 @@ export default class MatcheController {
     }
   };
 
-  public create = async (req: Request, res: Response, _next: NextFunction) => {
+  public create = async (req: Request, res: Response, next: NextFunction) => {
     const { authorization } = req.headers;
     try {
       auth(authorization);
       const result = await this.matcheService.create(req.body);
-      return res.status(200).send(result);
-    } catch {
-      throw new CustomError(404, 'authorization is undefined');
+      return res.status(201).send(result);
+    } catch (error) {
+      next(error);
     }
   };
 }
