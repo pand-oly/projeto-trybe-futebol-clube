917cf9f390ad7f04cb8b6e28040e1d902b33df6a
pand-oly <hoa98hoa@gmail.com>
2022-10-07 09:12:37 -0300

    refactore: remove .only

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -21,7 +21,7 @@ const RETURN_USER_MOCK = {
   password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
 }
 
-describe.only('Test login routes', () => {
+describe('Test login routes', () => {
   let chaiHttpResponse: Response;
 
   beforeEach(() => sinon.restore());
