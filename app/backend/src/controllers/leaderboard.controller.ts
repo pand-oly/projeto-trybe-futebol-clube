dfc2a38bccf18442108582e29ab6b99b7583c0ec
pand-oly <hoa98hoa@gmail.com>
2022-10-11 11:19:00 -0300

    feat: Created leaderboard service and controller

---
+++ app/backend/src/controllers/leaderboard.controller.ts
@@ -0,0 +1,5 @@
+import LeaderboardService from '../services/leaderboard.service';
+
+export default class LeaderboardController {
+  constructor(private leaderboardService: LeaderboardService) {}
+}
---
+++ app/backend/src/services/leaderboard.service.ts
@@ -0,0 +1,5 @@
+import MatcheModel from '../model/matche.model';
+
+export default class LeaderboardService {
+  constructor(private matcheModel: MatcheModel) {}
+}
